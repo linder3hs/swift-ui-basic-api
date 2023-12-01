687ec5354ac52e0e1b46db8c66fd17bc7032e7b0
linder3hs <linderhassinger00@gmail.com>
2023-11-30 23:55:21 -0500

    Initial Commit

---
+++ test-api-rest/test_api_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_api_restApp.swift
+//  test-api-rest
+//
+//  Created by Linder Anderson Hassinger Solano    on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct test_api_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
